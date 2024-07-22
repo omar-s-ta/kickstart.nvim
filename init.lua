862add390e3e1f88e956fe2082890a9c6fd72f5a
omar <omar@macbook-pro.fritz.box>
2024-07-22 14:38:44 +0200

    separate which-key plugin

--- init.lua
+++ init.lua
@@ -1009,6 +1009,7 @@ require('lazy').setup({
   require 'kickstart.plugins.copilot',
   require 'kickstart.plugins.copilot-cmp',
   require 'kickstart.plugins.lualine',
+  require 'kickstart.plugins.which-key',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
