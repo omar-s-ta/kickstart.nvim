95d1d07b2a16a84f523619ea2f37255086c0a931
omar-s-ta <omar.sebres@gmail.com>
2024-11-15 13:04:48 +0100

    use copilot from LazyExtras

--- init.lua
+++ init.lua
@@ -1031,8 +1031,6 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
-  require 'kickstart.plugins.copilot',
-  require 'kickstart.plugins.copilot-cmp',
   require 'kickstart.plugins.lualine',
   require 'kickstart.plugins.markdown-preview',
   require 'kickstart.plugins.render-markdown',
