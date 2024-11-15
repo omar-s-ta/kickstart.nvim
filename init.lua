7326b098999f03e84edc6d52a91fe3ea2f3c1650
omar-s-ta <omar.sebres@gmail.com>
2024-11-15 12:57:03 +0100

    remove taplo from Mason

--- init.lua
+++ init.lua
@@ -653,21 +653,6 @@ require('lazy').setup({
         -- ts_ls = {},
         --
         marksman = {},
-        taplo = {
-          keys = {
-            {
-              'K',
-              function()
-                if vim.fn.expand '%:t' == 'Cargo.toml' and require('crates').popup_available() then
-                  require('crates').show_popup()
-                else
-                  vim.lsp.buf.hover()
-                end
-              end,
-              desc = 'Show Crate Documentation',
-            },
-          },
-        },
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
