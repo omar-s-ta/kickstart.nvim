b853ec45e4103cba4151bd3dffae7d48b7fa5049
omar-s-ta <omar.sebres@gmail.com>
2024-11-28 20:03:29 +0100

    include lazy extras in core.lua

--- lua/kickstart/plugins/core.lua
+++ lua/kickstart/plugins/core.lua
@@ -7,6 +7,28 @@ return {
     },
   },
 
+  { import = 'lazyvim.plugins.extras.ai.copilot' },
+  { import = 'lazyvim.plugins.extras.ai.copilot-chat' },
+  { import = 'lazyvim.plugins.extras.coding.luasnip' },
+  { import = 'lazyvim.plugins.extras.coding.yanky' },
+  { import = 'lazyvim.plugins.extras.dap.nlua' },
+  { import = 'lazyvim.plugins.extras.editor.mini-files' },
+  { import = 'lazyvim.plugins.extras.formatting.prettier' },
+  { import = 'lazyvim.plugins.extras.lang.clangd' },
+  { import = 'lazyvim.plugins.extras.lang.cmake' },
+  { import = 'lazyvim.plugins.extras.lang.docker' },
+  { import = 'lazyvim.plugins.extras.lang.git' },
+  { import = 'lazyvim.plugins.extras.lang.helm' },
+  { import = 'lazyvim.plugins.extras.lang.json' },
+  { import = 'lazyvim.plugins.extras.lang.markdown' },
+  { import = 'lazyvim.plugins.extras.lang.rust' },
+  { import = 'lazyvim.plugins.extras.lang.scala' },
+  { import = 'lazyvim.plugins.extras.lang.toml' },
+  { import = 'lazyvim.plugins.extras.lang.yaml' },
+  { import = 'lazyvim.plugins.extras.linting.eslint' },
+  { import = 'lazyvim.plugins.extras.ui.treesitter-context' },
+  { import = 'lazyvim.plugins.extras.util.dot' },
+
   -- Detect tabstop and shiftwidth automatically
   {
     'tpope/vim-sleuth',
