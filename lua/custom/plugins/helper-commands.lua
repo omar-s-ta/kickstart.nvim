15eede599b1df670579f8504b7fba739f93b9546
omar-s-ta <omar.sebres@gmail.com>
2024-11-28 13:48:42 +0100

    include the size of the list in the print fn

--- lua/custom/plugins/helper-commands.lua
+++ lua/custom/plugins/helper-commands.lua
@@ -1,5 +1,5 @@
 local function print_table_with_text(text, tbl)
-  print(text .. '\n' .. table.concat(tbl, '\n'))
+  print(text .. ' (' .. #tbl .. ')\n' .. table.concat(tbl, '\n'))
 end
 
 vim.api.nvim_create_user_command('TSInstalled', function()
@@ -7,13 +7,6 @@ vim.api.nvim_create_user_command('TSInstalled', function()
   local installed_parsers = treesitter_info.installed_parsers()
 
   print_table_with_text('installed_parsers:', installed_parsers)
-end, {})
-
-vim.api.nvim_create_user_command('MasonInstalled', function()
-  local mason_registry = require 'mason-registry'
-  local installed_packages = mason_registry.get_installed_package_names()
-
-  print_table_with_text('installed_packages:', installed_packages)
 end, {})
 
 return {}
