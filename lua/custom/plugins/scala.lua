fc826c13820b068aa4e727491e74a17a4a75bf00
omar-s-ta <omar.sebres@gmail.com>
2024-11-28 19:27:06 +0100

    remove all LazyExtras

--- lua/custom/plugins/scala.lua
+++ lua/custom/plugins/scala.lua
@@ -1,80 +1,80 @@
 return {
-  {
-    'scalameta/nvim-metals',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'j-hui/fidget.nvim', opts = {} },
-    },
-    ft = { 'scala', 'sbt', 'java' },
-    opts = function()
-      local metals_config = require('metals').bare_config()
-      metals_config.settings = { showImplicitArguments = true }
-      metals_config.init_options.statusBarProvider = 'off'
-      metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
-      metals_config.on_attach = function(client, bufnr)
-        require('metals').setup_dap()
-
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
-        end
-
-        map('<leader>mh', require('metals').hover_worksheet, 'Metals hover worksheet')
-        map('<leader>mc', require('metals').compile_cascade, 'Metals compile cascade')
-        map('<leader>me', require('telescope').extensions.metals.commands, 'Metals commands')
-        map('<leader>in', require('telescope.builtin').lsp_incoming_calls, '[I]ncoming Calls')
-        map('<leader>out', require('telescope.builtin').lsp_outgoing_calls, '[O]utgoing Calls')
-
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = bufnr,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = bufnr,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
-
-          vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-            end,
-          })
-        end
-
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
-          end, '[T]oggle Inlay [H]ints')
-        end
-      end
-      return metals_config
-    end,
-    config = function(self, metals_config)
-      local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = self.ft,
-        callback = function()
-          require('metals').initialize_or_attach(metals_config)
-        end,
-        group = nvim_metals_group,
-      })
-    end,
-  },
+  -- {
+  --   'scalameta/nvim-metals',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     { 'j-hui/fidget.nvim', opts = {} },
+  --   },
+  --   ft = { 'scala', 'sbt', 'java' },
+  --   opts = function()
+  --     local metals_config = require('metals').bare_config()
+  --     metals_config.settings = { showImplicitArguments = true }
+  --     metals_config.init_options.statusBarProvider = 'off'
+  --     metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+  --     metals_config.on_attach = function(client, bufnr)
+  --       require('metals').setup_dap()
+  --
+  --       local map = function(keys, func, desc, mode)
+  --         mode = mode or 'n'
+  --         vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
+  --       end
+  --
+  --       map('<leader>mh', require('metals').hover_worksheet, 'Metals hover worksheet')
+  --       map('<leader>mc', require('metals').compile_cascade, 'Metals compile cascade')
+  --       map('<leader>me', require('telescope').extensions.metals.commands, 'Metals commands')
+  --       map('<leader>in', require('telescope.builtin').lsp_incoming_calls, '[I]ncoming Calls')
+  --       map('<leader>out', require('telescope.builtin').lsp_outgoing_calls, '[O]utgoing Calls')
+  --
+  --       map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  --       map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  --       map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  --       map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+  --       map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  --       map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  --       map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  --       map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+  --       map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  --
+  --       if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+  --         local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+  --         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+  --           buffer = bufnr,
+  --           group = highlight_augroup,
+  --           callback = vim.lsp.buf.document_highlight,
+  --         })
+  --
+  --         vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+  --           buffer = bufnr,
+  --           group = highlight_augroup,
+  --           callback = vim.lsp.buf.clear_references,
+  --         })
+  --
+  --         vim.api.nvim_create_autocmd('LspDetach', {
+  --           group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+  --           callback = function(event2)
+  --             vim.lsp.buf.clear_references()
+  --             vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+  --           end,
+  --         })
+  --       end
+  --
+  --       if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+  --         map('<leader>th', function()
+  --           vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
+  --         end, '[T]oggle Inlay [H]ints')
+  --       end
+  --     end
+  --     return metals_config
+  --   end,
+  --   config = function(self, metals_config)
+  --     local nvim_metals_group = vim.api.nvim_create_augroup('nvim-metals', { clear = true })
+  --     vim.api.nvim_create_autocmd('FileType', {
+  --       pattern = self.ft,
+  --       callback = function()
+  --         require('metals').initialize_or_attach(metals_config)
+  --       end,
+  --       group = nvim_metals_group,
+  --     })
+  --   end,
+  -- },
 }
