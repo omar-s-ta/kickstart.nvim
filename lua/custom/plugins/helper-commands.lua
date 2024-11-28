c3e4f807f3881b8c16a12527facbb8a21fc86f31
omar-s-ta <omar.sebres@gmail.com>
2024-11-28 13:38:59 +0100

    MasonInstalled helper command

--- lua/custom/plugins/helper-commands.lua
+++ lua/custom/plugins/helper-commands.lua
@@ -1,8 +1,19 @@
+local function print_table_with_text(text, tbl)
+  print(text .. '\n' .. table.concat(tbl, '\n'))
+end
+
 vim.api.nvim_create_user_command('TSInstalled', function()
   local treesitter_info = require 'nvim-treesitter.info'
   local installed_parsers = treesitter_info.installed_parsers()
 
-  print('installed_parsers:\n' .. table.concat(installed_parsers, '\n'))
+  print_table_with_text('installed_parsers:', installed_parsers)
+end, {})
+
+vim.api.nvim_create_user_command('MasonInstalled', function()
+  local mason_registry = require 'mason-registry'
+  local installed_packages = mason_registry.get_installed_package_names()
+
+  print_table_with_text('installed_packages:', installed_packages)
 end, {})
 
 return {}
