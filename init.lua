980d79b46565d13dffbfa232c5a2d1ec881580e1
omar-s-ta <omar.sebres@gmail.com>
2024-11-29 15:20:17 +0100

    new folder to organize custom plugings

--- init.lua
+++ init.lua
@@ -149,7 +149,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 
-  require 'kickstart.plugins.core',
+  require 'kickstart.plugins.after.core',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
