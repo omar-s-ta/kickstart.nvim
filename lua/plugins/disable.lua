cf5d25d23d28986c37c5fc7e6964f94252040de9
omar-s-ta <omar.sebres@gmail.com>
2024-12-03 13:27:38 +0100

    disable <leader><space> for telescope

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,4 +1,11 @@
 return {
   { 'saghen/blink.cmp', enabled = false },
   { 'garymjr/nvim-snippets', enabled = false },
+
+  {
+    'nvim-telescope/telescope.nvim',
+    keys = {
+      { '<leader><space>', false },
+    },
+  },
 }
