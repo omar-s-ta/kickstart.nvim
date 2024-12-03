829a1658acaf4ef2209bfff778804c9b7978e51a
omar-s-ta <omar.sebres@gmail.com>
2024-12-03 16:59:44 +0100

    disable copilot and remove redundant highlight on yank

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -124,13 +124,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
 vim.api.nvim_create_user_command('OptionsCheck', function()
   print 'options.lua'
