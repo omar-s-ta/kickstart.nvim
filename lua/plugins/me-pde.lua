d5d7038c2093f353625d7b3824f3ac69e4eed3fa
omar-s-ta <omar.sebres@gmail.com>
2024-12-03 16:23:16 +0100

    include LazyVim recipes

--- lua/plugins/me-pde.lua
+++ lua/plugins/me-pde.lua
@@ -44,6 +44,66 @@ return {
       { 'j-hui/fidget.nvim', opts = {} },
       'hrsh7th/cmp-nvim-lsp',
     },
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { 'utf-16' }
+        end,
+        eslint = function()
+          require('lazyvim.util').lsp.on_attach(function(client)
+            if client.name == 'eslint' then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == 'tsserver' then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-emoji' },
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+      end
+
+      table.insert(opts.sources, { name = 'emoji' })
+
+      local cmp = require 'cmp'
+      opts.mapping = vim.tbl_extend('force', opts.mapping or {}, {
+        ['<CR>'] = nil, -- Disable the <CR> keymap
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.snippet.active { direction = 1 } then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active { direction = -1 } then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      })
+    end,
   },
 
   {
