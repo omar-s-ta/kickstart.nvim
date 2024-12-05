17ca20dae84b026ed3f4cc7747e96ed409f7cc8a
omar-s-ta <omar.sebres@gmail.com>
2024-12-05 16:02:48 +0100

    remap 'gg' to 'gg0' and some organizing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,45 +1,36 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+--  Vim convenience keymaps
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page' })
 vim.keymap.set('n', '<leader>se', '/', { desc = 'Search current file' })
+vim.keymap.set('n', 'gg', 'gg0', { desc = 'Move to the first position in current file' })
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
--- vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
---
--- Resize window using <ctrl><shift> arrow keys
--- Overrides default keymaps
+--  Resize window using <ctrl><shift> arrow keys
+--  Overrides default keymaps
 vim.keymap.set('n', '<C-S-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
 vim.keymap.set('n', '<C-S-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
 vim.keymap.set('n', '<C-S-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
 vim.keymap.set('n', '<C-S-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
 
--- lua convenience keymaps
+--  Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+--  Lua convenience keymaps
 vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>', { desc = 'Source current file' })
 vim.keymap.set('n', '<space>x', ':.lua<CR>', { desc = 'Run current lua code' })
 vim.keymap.set('v', '<space>x', ':lua<CR>', { desc = 'Run highlighted lua codes' })
 
--- vim convenience keymaps
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down half page' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up half page' })
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+-- vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
