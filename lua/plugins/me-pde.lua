e7a38f18f7319dfdf81ee77735f6d515954097ef
omar-s-ta <omar.sebres@gmail.com>
2025-05-21 15:38:13 +0200

    include neotest-scala

--- lua/plugins/me-pde.lua
+++ lua/plugins/me-pde.lua
@@ -52,9 +52,29 @@ return {
   {
     'nvim-neotest/neotest',
     dependencies = {
+      'stevanmilic/neotest-scala',
+      'nvim-neotest/nvim-nio',
       'nvim-lua/plenary.nvim',
       'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+      adapters = {
+        ['neotest-scala'] = {
+          -- Command line arguments for runner
+          -- Can also be a function to return dynamic values
+          args = { '--no-color' },
+          -- Runner to use. Will use bloop by default.
+          -- Can be a function to return dynamic value.
+          -- For backwards compatibility, it also tries to read the vim-test scala config.
+          -- Possibly values bloop|sbt.
+          runner = 'sbt',
+          -- Test framework to use. Will use utest by default.
+          -- Can be a function to return dynamic value.
+          -- Possibly values utest|munit|scalatest.
+          framework = 'munit',
+        },
+      },
     },
   },
 
