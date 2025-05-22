19b9541f2eb923ceeabd3d6adaaeaf0353ddb365
omar-s-ta <omar.sebres@gmail.com>
2025-05-22 12:55:25 +0200

    use snacks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,8 @@ vim.g.mapleader = ' '
 -- Overrides ~/.local/share/nvim/lazy/LazyVim/lua/config/options.lua
 vim.g.maplocalleader = ' '
 
+vim.g.lazyvim_picker = 'snacks'
+
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = 'pyright'
