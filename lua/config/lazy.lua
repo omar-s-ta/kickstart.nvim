a68afa901bef9701e64f0a68217f235f8b27aa1a
omar-s-ta <omar.sebres@gmail.com>
2025-05-22 12:48:06 +0200

    include more languages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -43,6 +43,7 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.dap.nlua' },
 
     { import = 'lazyvim.plugins.extras.editor.aerial' },
+    { import = 'lazyvim.plugins.extras.editor.dial' },
     { import = 'lazyvim.plugins.extras.editor.mini-files' },
 
     { import = 'lazyvim.plugins.extras.formatting.prettier' },
@@ -52,17 +53,23 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.lang.cmake' },
     { import = 'lazyvim.plugins.extras.lang.docker' },
     { import = 'lazyvim.plugins.extras.lang.git' },
+    { import = 'lazyvim.plugins.extras.lang.go' },
     { import = 'lazyvim.plugins.extras.lang.helm' },
     { import = 'lazyvim.plugins.extras.lang.java' },
     { import = 'lazyvim.plugins.extras.lang.json' },
     { import = 'lazyvim.plugins.extras.lang.kotlin' },
     { import = 'lazyvim.plugins.extras.lang.markdown' },
+    { import = 'lazyvim.plugins.extras.lang.nix' },
+    { import = 'lazyvim.plugins.extras.lang.ocaml' },
+    { import = 'lazyvim.plugins.extras.lang.python' },
     { import = 'lazyvim.plugins.extras.lang.rust' },
     { import = 'lazyvim.plugins.extras.lang.scala' },
+    { import = 'lazyvim.plugins.extras.lang.sql' },
     { import = 'lazyvim.plugins.extras.lang.terraform' },
     { import = 'lazyvim.plugins.extras.lang.toml' },
+    { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.yaml' },
-    { import = 'lazyvim.plugins.extras.lang.python' },
+    { import = 'lazyvim.plugins.extras.lang.zig' },
 
     { import = 'lazyvim.plugins.extras.linting.eslint' },
 
